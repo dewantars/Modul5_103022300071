befa53cdb96b1b678d9f032e7a7b2bdb3c6c05f4
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:23:18 +0700

    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class PemrosesData()
+{
+    public float DapatkanNilaiTerbesar<T>(T a, T b, T c)
+    {
+        dynamic nilaiTerbesar = (dynamic)a;
+        dynamic data = (dynamic)b;
+        dynamic data2 = (dynamic)c;
+
+        if (nilaiTerbesar < data)
+        {
+            nilaiTerbesar = data;
+        }
+        if (nilaiTerbesar < data2)
+        {
+            nilaiTerbesar = data2;
+        }
+        return nilaiTerbesar;
+    }
+
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        PemrosesData pemrosesData = new PemrosesData();
+        float nilaiTerbesar = pemrosesData.DapatkanNilaiTerbesar(1.0f, 3.0f, 2.2f);
+        Console.WriteLine("Nilai terbesar dari 1.0, 3.0, 2.2 adalah");
+        Console.WriteLine(nilaiTerbesar);
+    }
+}
